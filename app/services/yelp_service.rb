60e36d1bb18dc6486163475a680a13a20b7b2434
Kyle Cornelissen <46171611+kylecornelissen@users.noreply.github.com>
2019-07-29 09:47:27 -0600

    Add test for yelp service spec

--- app/services/yelp_service.rb
+++ app/services/yelp_service.rb
@@ -1,12 +1,11 @@
 class YelpService
   def initialize(location, arrival_time, cuisine)
-    @latitude = location
+    @location = location
     @arrival_time = arrival_time
     @cuisine = cuisine
   end
 
   def restaurant_options
-    require "pry"; binding.pry
     fetch_data
   end
 
@@ -15,7 +14,8 @@ class YelpService
   def conn
     Faraday.new(url: "https://api.yelp.com/v3/businesses/search") do |f|
       f.adapter Faraday.default_adapter
-      f.headers['Authorization'] = "Bearer #{ENV['YELP_API_KEY']}
+      f.headers['Authorization'] = "Bearer #{ENV['YELP_API_KEY']}"
+    # end
       f.params[:location] = @location
       f.params[:open_at] = @arrival_time
       f.params[:categories] = @cuisine
@@ -29,3 +29,19 @@ class YelpService
   end
 end
 #https://api.yelp.com/v3/businesses/search?location=pueblo,co&open_at=1564412184&categories=chinese&limit=3
+
+# def get_json(url, params = {})
+#       response = conn.get(url, params)
+#       JSON.parse(response.body, symbolize_names: true)
+# end
+
+# def conn
+#      Faraday.new(url: 'https://api.yelp.com') do |f|
+#          f.adapter Faraday.default_adapter
+#          f.headers['Authorization'] = "Bearer #{ENV['YELP_API_KEY']}
+#      end
+# end
+#
+# def conn
+#     Faraday.new('https://api.yelp.com', headers: {'Authorization' => "Bearer #{ENV['YELP_API_KEY']}"})
+# end
--- spec/services/yelp_service_spec.rb
+++ spec/services/yelp_service_spec.rb
@@ -2,21 +2,17 @@ require 'rails_helper'
 
 describe YelpService do
   it "exists" do
-    subject = YelpService.new("denver,co")
+    subject = YelpService.new("denver,co", 1564412184, "chinese")
     expect(subject).to be_a YelpService
   end
 
   context "#restaurant_options" do
     it "returns three restaurants that meet criteria" do
-      VCR.use_cassette("yelp_service") do
-        subject = YelpService.new("pueblo,co", 1564412184, "chinese")
-        restaurants = subject.restaurant_options
-        expect(restaurants).to be_a Hash
-        # expect(address_coordinates).to have_key :lat
-        # expect(address_coordinates).to have_key :lng
-        # expect(address_coordinates[:lat]).to be_a Float
-        # expect(address_coordinates[:lng]).to be_a Float
-      end
+      subject = YelpService.new("denver,co", 1564412184, "chinese")
+      restaurants = subject.restaurant_options
+      expect(restaurants).to be_a Hash
+      expect(restaurants).to have_key :businesses
+      expect(restaurants[:businesses].count).to eq(3)
     end
   end
 end

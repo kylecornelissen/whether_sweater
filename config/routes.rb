e976d09220df79b5259ff35b87d2de4be65309fe
Kyle Cornelissen <46171611+kylecornelissen@users.noreply.github.com>
2019-07-29 10:36:02 -0600

    Add munchies index route

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       get 'forecast', to: 'forecast#index'
+      # GET /api/v1/munchies?start=denver,co&end=pueblo,co&food=chinese
+      get 'munchies', to: 'munchies#index'
     end
   end
+
+
 end

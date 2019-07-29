d643bf30c94ba2bfa9fc77c8fa23194c93db5e0e
Kyle Cornelissen <46171611+kylecornelissen@users.noreply.github.com>
2019-07-29 10:55:55 -0600

    Refactor restaurants method in weather_facade

--- app/facades/munchies_facade.rb
+++ app/facades/munchies_facade.rb
@@ -16,13 +16,14 @@ class MunchiesFacade
     arrival_time = current_time + duration_time
   end
 
-  def restaurants
+  def find_restaurants
     restaurants = yelp_service(end_city, arrival_time, @params[:food]).restaurant_options[:businesses]
-    # require "pry"; binding.pry
-    restaturants =  { restaurants.first[:name] => restaurants.first[:location][:display_address],
-                      restaurants[1][:name] => restaurants[1][:location][:display_address],
-                      restaurants[2][:name] => restaurants[2][:location][:display_address]
-                    }
+  end
+
+  def restaurants
+    find_restaurants.map do |restaurant|
+      {restaurant[:name] => restaurant[:location]}
+    end
   end
 
   private

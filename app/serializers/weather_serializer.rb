e62a62af7e136ed683f48a21fcb93308a48f95ef
Kyle Cornelissen <46171611+kylecornelissen@users.noreply.github.com>
2019-07-30 14:43:25 -0600

    Remove current_details attribute from weather serializer

--- app/serializers/weather_serializer.rb
+++ app/serializers/weather_serializer.rb
@@ -1,4 +1,4 @@
 class WeatherSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :id, :current_weather, :current_details, :hourly_weather, :daily_weather
+  attributes :id, :current_weather, :hourly_weather, :daily_weather
 end

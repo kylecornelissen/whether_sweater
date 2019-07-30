4a05621c23caf220ae694fb098300603bbb7c4c6
Kyle Cornelissen <46171611+kylecornelissen@users.noreply.github.com>
2019-07-30 14:44:46 -0600

    Add VCR cassette to google geolocations service spec

--- spec/services/google_geo_service_spec.rb
+++ spec/services/google_geo_service_spec.rb
@@ -6,17 +6,22 @@ describe GoogleGeoService do
     expect(subject).to be_a GoogleGeoService
   end
 
-  context "#address_location" do
+  context "#location_coordinates" do
     it "returns latitude and longitude" do
-      VCR.use_cassette("google_geo_service") do
+      VCR.use_cassette('services/google_geo_service') do
         subject = GoogleGeoService.new("denver,co")
-        address_coordinates = subject.address_coordinates
-        expect(address_coordinates).to be_a Hash
-        expect(address_coordinates).to have_key :lat
-        expect(address_coordinates).to have_key :lng
-        expect(address_coordinates[:lat]).to be_a Float
-        expect(address_coordinates[:lng]).to be_a Float
+        location_coordinates = subject.location_coordinates
+        expect(location_coordinates).to be_a Hash
+        expect(location_coordinates).to have_key :lat
+        expect(location_coordinates).to have_key :lng
+        expect(location_coordinates[:lat]).to be_a Float
+        expect(location_coordinates[:lng]).to be_a Float
       end
+    end
+  end
+
+  context "#location_address" do
+    xit "returns latitude and longitude" do
     end
   end
 end

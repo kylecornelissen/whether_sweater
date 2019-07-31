472e97adad358dadc67941026c69fbc23e13ad33
Kyle Cornelissen <46171611+kylecornelissen@users.noreply.github.com>
2019-07-30 19:50:02 -0600

    Add seed user object

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+kyle = User.create!(email: 'kyle@gmail.com', password: 'password', api_key: 'seeded_api_key')

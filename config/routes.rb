6a3cb795a7dc33276be236dbff67051cb412d1ec
Kyle Cornelissen <46171611+kylecornelissen@users.noreply.github.com>
2019-07-31 11:04:54 -0600

    Add routes for road_trip#create

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
       get "logout", to: "sessions#destroy"
       get "sessions", to: "sessions#create"
       post "sessions", to: "sessions#create"
+      get "road_trip", to: "road_trip#create"
+      post "road_trip", to: "road_trip#create"
     end
   end
 end

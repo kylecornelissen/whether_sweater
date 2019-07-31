5845963a502b13e0f448da265f47fa0c80f4d56b
Kyle Cornelissen <46171611+kylecornelissen@users.noreply.github.com>
2019-07-31 07:56:50 -0600

    Add request spec for munchies controller

---
+++ spec/requests/api/v1/munchies_request_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+describe "Yelp API" do
+  it "gets start and end directions with food type and gives restaurants that are open" do
+    VCR.use_cassette('services/google_directions_service2') do
+      VCR.use_cassette('services/yelp_service2') do
+        get '/api/v1/munchies?start=denver,co&end=pueblo,co&food=breakfast'
+      end
+    end
+
+      expect(response).to be_successful
+      munchies = JSON.parse(response.body)["data"]["attributes"]
+
+      expect(munchies).to have_key("restaurants")
+      expect(munchies["restaurants"].count).to eq(3)
+      expect(munchies["restaurants"].first).to have_key("The Hanging Tree Cafe")
+      expect(munchies["restaurants"].first["The Hanging Tree Cafe"]).to have_key("address1")
+      expect(munchies["restaurants"].first["The Hanging Tree Cafe"]).to have_key("address2")
+      expect(munchies["restaurants"].first["The Hanging Tree Cafe"]).to have_key("address3")
+      expect(munchies["restaurants"].first["The Hanging Tree Cafe"]).to have_key("city")
+      expect(munchies["restaurants"].first["The Hanging Tree Cafe"]).to have_key("zip_code")
+      expect(munchies["restaurants"].first["The Hanging Tree Cafe"]).to have_key("country")
+      expect(munchies["restaurants"].first["The Hanging Tree Cafe"]).to have_key("state")
+  end
+end
